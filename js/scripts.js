d32307a7cb9ebeb472f956fb63a7e33c7327922d
Chad McKee <chadm_@yahoo.com>
2017-06-18 14:26:14 -0400

    scroll and project animations

--- js/scripts.js
+++ js/scripts.js
@@ -5,21 +5,63 @@
 
 $(document).ready(function () {
 
+    myCellular = 'NzA2LTk5Mi05NTYy';
+    myInbox = 'Y2hhZG1fQHlhaG9vLmNvbQ==';
+    $('#cell').click(function () {
 
-
-    $('#cell').hover(function () {
-        myCellular = 'NzA2LTk5Mi05NTYy';
-        $('.contact-box').html(atob(myCellular));
-        setTimeout(function () {
-            $('.contact-box').html('');
-        },5000)
+        $('.contact-box').html('<br>Phone: ' + atob(myCellular) + '<br><br> Email: ' + atob(myInbox));
+        $('.contact-box').fadeIn('slow');
     });
-    $('#mail').hover(function () {
-        myInbox = 'Y2hhZG1fQHlhaG9vLmNvbQ==';
-        $('.contact-box').html(atob(myInbox));
+    $('#mail').click(function () {
+        $('.contact-box').html('<br>Phone: ' + atob(myCellular) + '<br><br> Email: ' + atob(myInbox));
+        $('.contact-box').fadeIn();
+    });
+    $('#Projects').click(function () {
+       $('#Nav-Bar').fadeOut();
+        $('#About').fadeOut();
+        $('#Contact').fadeOut();
+        $('#Top').fadeOut();
+        $('#Skills').fadeOut();
+        $('#Links').fadeOut()
+    });
+    $('.cd-close').click(function () {
         setTimeout(function () {
-            $('.contact-box').html('');
-        },5000)
+            $('#Nav-Bar').fadeIn('slow');
+            $('#About').fadeIn();
+            $('#Contact').fadeIn();
+            $('#Top').fadeIn();
+            $('#Skills').fadeIn();
+            $('#Links').fadeIn();
+            $('html, body').animate({
+                scrollTop: $("#Projects").offset().top
+            }, 0);
+        },2500);
+    });
+
+    $('#About-link').click(function () {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#About").offset().top
+        }, 700);
+    });
+
+    $('#Top-link').click(function () {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#Top").offset().top
+        }, 700);
+    });
+    $('#Projects-link').click(function () {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#Projects").offset().top
+        }, 700);
+    });
+    $('#Contact-link').click(function () {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#Contact").offset().top
+        }, 700);
     });
 
 });
